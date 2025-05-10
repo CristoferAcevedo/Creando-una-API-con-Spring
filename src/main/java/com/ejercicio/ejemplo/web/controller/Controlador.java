476145a0806e8cfb723b5f69b540fe1d7ce43924
CristoferAcevedo <cristoferacevedo90@gmail.com>
2025-05-10 18:54:15 -0500

    acomodando estructura

--- src/main/java/com/ejercicio/ejemplo/web/controller/Controlador.java
+++ src/main/java/com/ejercicio/ejemplo/web/controller/Controlador.java
@@ -1,4 +1,4 @@
-package com.ejercicio.ejemplo;
+package com.ejercicio.ejemplo.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
